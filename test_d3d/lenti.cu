e8baeb374e8c4248aacc42ef76b0fb4405699bed
IChewiCreamable <ya.mynameisnanashinomi3@gmail.com>
2023-02-08 03:26:36 +0900

    範囲外を弄らないようにした&extern "C"化

--- test_d3d/lenti.cu
+++ test_d3d/lenti.cu
@@ -1,36 +1,42 @@
 ﻿
-/// @brief アルファチャンネルを加えて24bitを32bitにするやつ
-/// @param dst 展開先のsurface
-/// @param src 元データ
-/// @param memPitch ストライド
-/// @return
-__global__ void color24bitTo32bit(
-    cudaSurfaceObject_t dst, const char *src, int memPitch)
+extern "C"
 {
-    unsigned int x = blockDim.x * blockIdx.x + threadIdx.x;
-    unsigned int y = blockDim.y * blockIdx.y + threadIdx.y;
-    unsigned int srcidx = x + y * memPitch;
-    char4 data;
-    data.x = src[srcidx * 3];
-    data.y = src[srcidx * 3 + 1];
-    data.z = src[srcidx * 3 + 2];
-    surf2Dwrite<char4>(data, dst, x, y);
-}
-
-/// @brief 画像をレンチキュラー化するやつ
-/// @param dst ターゲット画像
-/// @param src ソース画像
-/// @param srcidx ソース画像の左から数えた番号
-/// @param srccnt ソース画像の数
-__global__ void writeLenti(
-    cudaSurfaceObject_t dst,
-    cudaSurfaceObject_t src,
-    int srcidx,
-    int srccnt)
-{
-    unsigned int x = blockDim.x * blockIdx.x + threadIdx.x;
-    unsigned int y = blockDim.y * blockIdx.y + threadIdx.y;
-    unsigned int dstx = x * srccnt + srcidx;
-    char4 data = surf2Dread<char4>(src, x, y);
-    surf2Dwrite(data, dst, dstx, y);
+    /// @brief アルファチャンネルを加えて24bitを32bitにするやつ
+    /// @param dst 展開先のsurface
+    /// @param src 元データ
+    /// @param memPitch ストライド
+    /// @return
+    __global__ void color24bitTo32bit(
+        cudaSurfaceObject_t dst, const char *src, unsigned int memPitch,
+        unsigned int width, unsigned int height)
+    {
+        unsigned int x = blockDim.x * blockIdx.x + threadIdx.x;
+        unsigned int y = blockDim.y * blockIdx.y + threadIdx.y;
+        if (x < width && y < height)
+        {
+            unsigned int srcidx = x + y * memPitch;
+            char4 data;
+            data.x = src[srcidx * 3];
+            data.y = src[srcidx * 3 + 1];
+            data.z = src[srcidx * 3 + 2];
+            surf2Dwrite<char4>(data, dst, x, y);
+        }
+    }
+    /// @brief 画像をレンチキュラー化するやつ
+    /// @param dst ターゲット画像
+    /// @param src ソース画像
+    /// @param srcidx ソース画像の左から数えた番号
+    /// @param srccnt ソース画像の数
+    __global__ void writeLenti(
+        cudaSurfaceObject_t dst,
+        cudaSurfaceObject_t src,
+        int srcidx,
+        int srccnt)
+    {
+        unsigned int x = blockDim.x * blockIdx.x + threadIdx.x;
+        unsigned int y = blockDim.y * blockIdx.y + threadIdx.y;
+        unsigned int dstx = x * srccnt + srcidx;
+        char4 data = surf2Dread<char4>(src, x, y);
+        surf2Dwrite(data, dst, dstx, y);
+    }
 }
